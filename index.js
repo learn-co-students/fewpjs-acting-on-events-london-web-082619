0fce6fad6adb8154d4e3ba282c17c687f922f1b8
Abdullah Bajaman <ambajaman@gmail.com>
2019-10-07 16:37:18 +0100

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 // Your code here
 let dodger = document.getElementById("dodger");
 
-// dodger.style.left = '180px'
-// dodger.style.bottom = '0px'
-
-// document.addEventListener('keydown', e=>{
-//     if(e.key === 'ArrowLeft'){
-//         let leftNumbers = dodger.style.left.replace("px", '');
-//         let left = parseInt(leftNumbers, 10)
-
-//         dodger.style.left = `${left - 1}px`
-//     }
-// })
 function moveDodgerLeft(){
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
